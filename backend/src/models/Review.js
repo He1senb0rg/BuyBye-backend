8742d6c771eead6ad7e4cb61b13821f8df7fd168
He1senb0rg <andresantosescola2020@gmail.com>
2025-04-22 16:46:28 +0100

    feat: added product endpoints, get all, get by id, update, create and delete

---
+++ backend/src/models/Review.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const ReviewSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default model('Review', ReviewSchema);
