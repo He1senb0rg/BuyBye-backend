52bd0912a56159679187422019f40a1db622c66a
He1senb0rg <andresantosescola2020@gmail.com>
2025-05-01 14:21:10 +0100

    feat: added sizes and colors to product schema

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,69 +1,90 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-const DiscountSchema = new Schema({
+const DiscountSchema = new Schema(
+  {
     type: {
       type: String,
-      enum: ['percentage', 'fixed'],
-      required: true
+      enum: ["percentage", "fixed"],
+      required: true,
     },
     value: {
       type: Number,
-      required: true
+      required: true,
     },
     start_date: {
       type: Date,
     },
     end_date: {
       type: Date,
-    }
-  }, { _id: false });
+    },
+  },
+  { _id: false }
+);
+
+const colorSchema = Schema(
+  {
+    name: String,
+    hex: String,
+  },
+  { _id: false }
+);
+
+const sizeSchema = Schema(
+  {
+    name: String,
+    size: String,
+  },
+  { _id: false }
+);
 
 const ProductSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  discount: {
+    type: DiscountSchema,
+    default: null,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  colors: [colorSchema],
+  sizes: [sizeSchema],
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    description: {
-        type: String,
+  ],
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  category: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
     },
-    price: {
-        type: Number,
-        required: true,
+  ],
+  images: [
+    {
+      type: String,
+      required: true,
     },
-    discount: {
-        type: DiscountSchema,
-        default: null
-      },
-    stock: {
-        type: Number,
-        required: true,
-    },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ],
-    averageRating: {
-        type: Number,
-        default: 0
-    },
-    category: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Category'
-        }
-    ],
-    images: [
-        {
-            type: String,
-            required: true
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export default model('Product', ProductSchema);
+export default model("Product", ProductSchema);
