5b6eb970819131dce865c4a63faea9897121d2d7
Guilherme Sardinha <83502530+william-217@users.noreply.github.com>
2025-05-07 00:10:16 +0100

    wip: cart schemas finished

--- backend/src/models/Cart.js
+++ backend/src/models/Cart.js
@@ -1,5 +1,29 @@
 import { Schema, model } from "mongoose";
 
+const CartSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
+  items: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      name: String,
+      price: Number,
+      image: String,
+      quantity: { type: Number, default: 1 },
+      wishlisted: { type: Boolean, default: false },
+      addedAt: { type: Date, default: Date.now }
+    }
+  ],
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
+module.exports = mongoose.model('Cart', CartSchema);
 
 export default model("Cart", Cart);
